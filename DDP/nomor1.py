59f2c1afbdabd11c64aebb9431dfcb3eba602088
shisysn-lgtm <shisy.sn@gmail.com>
2025-11-19 11:14:14 +0700

    Add files via upload

---
+++ DDP/nomor1.py
@@ -0,0 +1,4 @@
+def celcius_ke_fahrenheit(c):
+    return(c*9/5)+32
+
+print(celcius_ke_fahrenheit(37))
---
+++ DDP/nomor3.py
@@ -0,0 +1,10 @@
+def nilai(n = 0):
+    if n <= 60:
+        print("tidak lulus")
+    elif n > 60 and n <= 100:
+        print ("lulus")
+    else:
+        print("tidak lulus")
+
+nilai(80)
+nilai()
