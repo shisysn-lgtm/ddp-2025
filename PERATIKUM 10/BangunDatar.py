02fa588de97968e818d58e5f2dcc33e8718adcba
shisysn-lgtm <shisy.sn@gmail.com>
2025-11-26 12:25:01 +0700

    Add files via upload

---
+++ PERATIKUM 10/BangunDatar.py
@@ -0,0 +1,17 @@
+import math
+def jajarganjar(alas, tinggi):
+    return alas * tinggi
+
+def persegi(sisi):
+    return sisi * sisi
+
+
+def persegipanjang(panjang, lebar):
+    return panjang * lebar
+
+def segitiga(alas, tinggi):
+    return 0,5 * alas * tinggi
+
+def lingkaran(jari_jari):
+    return jari_jari ** 2
+
---
+++ PERATIKUM 10/panggil.py
@@ -0,0 +1,21 @@
+import bangunRuang as br
+import BangunDatar as bd
+
+print("----BANGUN RUANG----")
+print(f"volum kubus dengan sisi 3 adalah {br.kubus(3)}")
+print(f"volume balok adalah {br.balok(4, 5, 2)}")
+print(f"volume prisma segitiga adalah {br.balok(5,4,5)}")
+print(f"volume tabung adalah")
+print(f"volume kerucut")
+
+print("----BANGUN DATAR----")
+print(f"luas persegi adalah{bd.persegi(4)}")
+print(f"luas persegi panjang adalah{bd.persegipanjang(2,4)}")
+print(f"luas segitiga adalah{bd.segitiga(3,2)}")
+print(f"luas jajar ganjar adalah{bd.jajarganjar(2,2)}")
+print(f"luas lingkaran adalah{bd.lingkaran(5)}")
+
+
+
+
+ 
